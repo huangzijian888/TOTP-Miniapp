4314a05266eea2ed40b37cd051dce5f59e08354f
huangzijian <huangzijian@lamdaer.com>
2020-11-21 21:26:23 +0800

    feat(util): 增加获取参数及 account 的方法

--- utils/util.js
+++ utils/util.js
@@ -4,6 +4,20 @@ function getSeconds() {
     return now.getSeconds()
 }
 
+function getQueryByName(url, name) {
+    const reg = new RegExp('[?&]' + name + '=([^&#]+)');
+    const query = url.match(reg);
+    return query ? query[1] : null;
+}
+
+function getAccount(url, issuer) {
+    const reg = new RegExp('\\S+' + issuer + ':(\\S+)\\?')
+    const account = url.match(reg);
+    return account ? account[1] : null
+}
+
 module.exports = {
     getSeconds: getSeconds,
+    getQueryByName: getQueryByName,
+    getAccount: getAccount
 }
