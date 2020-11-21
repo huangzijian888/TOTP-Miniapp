2cff2b683b95868417f2cb297f1452baa1d3b4b5
huangzijian <huangzijian@lamdaer.com>
2020-11-21 22:18:32 +0800

    style(util): 完善注释

--- utils/util.js
+++ utils/util.js
@@ -1,15 +1,30 @@
-// 获取当前时间秒
+/**
+ * 获取当前时间的秒值
+ * @returns {number} 当前时间的秒值
+ */
 function getSeconds() {
     let now = new Date()
     return now.getSeconds()
 }
 
+/**
+ * 获取参数
+ * @param url totp 链接
+ * @param name 参数名
+ * @returns 参数值
+ */
 function getQueryByName(url, name) {
     const reg = new RegExp('[?&]' + name + '=([^&#]+)');
     const query = url.match(reg);
     return query ? query[1] : null;
 }
 
+/**
+ * 获取账户信息
+ * @param url totp 链接
+ * @param issuer 发布平台
+ * @returns 账户信息
+ */
 function getAccount(url, issuer) {
     const reg = new RegExp('\\S+' + issuer + ':(\\S+)\\?')
     const account = url.match(reg);
